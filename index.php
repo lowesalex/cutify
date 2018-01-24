b86b2740bdb7ee887dde7bc3dafe92645fef96f9
Daniel Alexander Lowes <lowesalex@gmail.com>
2018-01-24 22:51:43 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="generator"
+    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
+    <meta charset="UTF-8" />
+    <title>Cutify</title>
+  </head>
+  <body>
+    <h1 class="indexheading">Wellcome to cutify</h1>
+    <p>Please fill in the details</p>
+    <form action="">
+      <label for="">phone
+      <br><input type="text" class="phone" name="phone" />
+      <br><label for="message" >message
+      <br><input maxlength="160" size="160" type="text" class="message" name="message" /></label></label>
+    </form>
+  </body>
+</html>
