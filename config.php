6d0e6c4989b4321cf1dc7a2a9254a38facb8e84e
Daniel Alexander Lowes <lowesalex@gmail.com>
2018-01-25 16:23:36 +1100

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,4 @@
 /* API configuration variables file with real data must be hidden from public */
 $siddata = "enter your siddata here" 
 $authtoken="enter your auth token here"
-
-
-$sid = echo "$siddata"; //get Sid from your twilio.com/user/account
-$token = echo "$authtoken";  //get Auth Token from twilio.com/user/account
-
  ?>
